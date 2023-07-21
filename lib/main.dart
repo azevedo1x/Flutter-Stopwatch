632c2d9c90c0a1667aa3833dcae0ef6b99013ddd
gabrielsazevedo <gabriel.azeve04@gmail.com>
2023-07-21 11:33:05 -0300

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,41 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, must_be_immutable
 
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
     ProviderScope(
-        child: MaterialApp(
-      home: Inicio(),
-    )),
+      child: MaterialApp(
+        home: Inicio(),
+      ),
+    ),
   );
 }
 
 final cronometroProvider = StateProvider((ref) => false);
+final elapsedProvider = StateProvider((ref) => Duration.zero);
 
 class Inicio extends ConsumerWidget {
+  Stopwatch stopwatch = Stopwatch();
+
+  Future cronometroAction(WidgetRef ref) async {
+    while (ref.watch(cronometroProvider)) {
+      elapsed = stopwatch.elapsed;
+      await Future.delayed(const Duration(seconds: 1));
+      
+    }
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     bool isOn = ref.watch(cronometroProvider);
-    int tempo = 000000;
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Cronômetro Musical'),
       ),
-
-      //me ajude a colocar o cronometro aqui
       body: Center(
         child: Column(
           children: [
@@ -34,11 +44,19 @@ class Inicio extends ConsumerWidget {
               width: 250,
               height: 250,
             ),
-            Text(tempo.toString()),
+            Text(elapsed.toString()),
+
             ElevatedButton(
-                onPressed: () {
-                  ref.read(cronometroProvider.notifier).state = !isOn;
-                },
+              onPressed: () {
+                ref.read(cronometroProvider.notifier).state = !isOn;
+
+                if (isOn == true) {
+                  stopwatch.start();
+                  cronometroAction(ref);
+                } else {
+                  stopwatch.stop();
+                }
+              },
                 child: Text('Iniciar/Pausar')),
           ],
         ),
