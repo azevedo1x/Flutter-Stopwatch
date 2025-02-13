7ad6e755590ddf67633d7bc0689c1751bcf1db96
Gabriel Azevedo <gabriel.azevedo@branef.com.br>
2025-02-13 15:52:19 -0300

    upgrading project config

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,14 +6,15 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cronometro_musical/main.dart';
+import 'package:cronometro/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(ProviderScope(child: MaterialApp(home: Inicio())));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
